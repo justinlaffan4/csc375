9d30e2b4dc283426b585cd846e417b4e8c6b5f13
Justin Laffan <justinlaffan4@gmail.com>
2025-09-25 19:48:38 -0400

    For debugging the changes I've added back the old path finding algorithm

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -32,6 +32,91 @@ unsigned int random(unsigned int *rng_seed)
 	return x;
 }
 
+struct Font
+{
+	float  baseline_advance;
+	GLuint texture;
+
+	stbtt_packedchar char_data['~' - ' '];
+};
+
+const int BITMAP_W = 512;
+const int BITMAP_H = 512;
+
+unsigned char tmp_bitmap[BITMAP_W * BITMAP_H];
+unsigned char ttf_buf[1 << 25];
+
+Font load_font(const char *filename)
+{
+	Font result = {};
+
+	FILE *file = fopen(filename, "rb");
+	if(file)
+	{
+		fread(ttf_buf, sizeof(char), array_count(ttf_buf), file);
+
+		stbtt_fontinfo font_info;
+		if(stbtt_InitFont(&font_info, ttf_buf, stbtt_GetFontOffsetForIndex(ttf_buf, 0)))
+		{
+			int   font_pixel_size = 16;
+			float font_scale      = stbtt_ScaleForPixelHeight(&font_info, font_pixel_size);
+
+			int ascent, descent, line_gap;
+			stbtt_GetFontVMetrics(&font_info, &ascent, &descent, &line_gap);
+
+			result.baseline_advance = font_scale * (ascent - descent + line_gap);
+
+			stbtt_pack_context pack_ctx;
+			stbtt_PackBegin(&pack_ctx, tmp_bitmap, BITMAP_W, BITMAP_H, 0, 1, NULL);
+			stbtt_PackSetOversampling(&pack_ctx, 2, 2);
+			stbtt_PackFontRange(&pack_ctx, ttf_buf, 0, font_pixel_size, ' ', array_count(result.char_data), result.char_data);
+			stbtt_PackEnd(&pack_ctx);
+
+			glGenTextures(1, &result.texture);
+			glBindTexture(GL_TEXTURE_2D, result.texture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_W, BITMAP_H, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tmp_bitmap);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		}
+
+		fclose(file);
+	}
+
+	return result;
+}
+
+void draw_text(Font *font, float x, float y, float r, float g, float b, char *text)
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, font->texture);
+
+	glBegin(GL_QUADS);
+
+	glColor3f(r, g, b);
+
+	while(*text)
+	{
+		stbtt_aligned_quad quad;
+		stbtt_GetPackedQuad(font->char_data, BITMAP_W, BITMAP_H, *text++ - ' ', &x, &y, &quad, false);
+
+		glTexCoord2f(quad.s0, quad.t0); glVertex2f(quad.x0, quad.y0);
+		glTexCoord2f(quad.s1, quad.t0); glVertex2f(quad.x1, quad.y0);
+		glTexCoord2f(quad.s1, quad.t1); glVertex2f(quad.x1, quad.y1);
+		glTexCoord2f(quad.s0, quad.t1); glVertex2f(quad.x0, quad.y1);
+	}
+	glEnd();
+}
+
+void draw_rect(float x, float y, float w, float h, float r, float g, float b)
+{
+	glColor3f(r, g, b);
+
+	glVertex2f(x,     y    );
+	glVertex2f(x + w, y    );
+	glVertex2f(x + w, y + h);
+	glVertex2f(x,     y + h);
+}
+
 struct AStarNode
 {
 	int x;
@@ -201,6 +286,167 @@ AStarBinaryHeapNode *a_star_node_insert(AStarBinaryHeapNode *parent, AStarNode *
 	return result;
 }
 
+const int MAP_W = 128;
+const int MAP_H = 128;
+
+int map[MAP_H][MAP_W];
+
+AStarNode *a_star_path_find_new(int start_x, int start_y, int target_x, int target_y)
+{
+	AStarNode *result = NULL;
+
+	static AStarNode a_star_node_map[MAP_H][MAP_W];
+	memset(a_star_node_map, 0, MAP_W * MAP_H * sizeof(AStarNode));
+
+	a_star_node_map[start_y][start_x] = {start_x, start_y};
+
+	AStarBinaryHeapNode *to_search = (AStarBinaryHeapNode *)calloc(1, sizeof(AStarBinaryHeapNode));
+	to_search->n = &a_star_node_map[start_y][start_x];
+
+	while(to_search)
+	{
+		AStarNode *curr = to_search->n;
+
+		if(curr->x == target_x && curr->y == target_y)
+		{
+			result = curr;
+			break;
+		}
+
+		curr->visited = true;
+		curr->search  = false;
+
+		to_search = a_star_node_remove(to_search);
+
+		int neighbor_offsets_x[] = {1, 0, -1,  0};
+		int neighbor_offsets_y[] = {0, 1,  0, -1};
+
+		for(int neighbor_idx = 0; neighbor_idx < 4; ++neighbor_idx)
+		{
+			int neighbor_x = curr->x + neighbor_offsets_x[neighbor_idx];
+			int neighbor_y = curr->y + neighbor_offsets_y[neighbor_idx];
+
+			if(neighbor_x >= 0 && neighbor_x < MAP_W && neighbor_y >= 0 && neighbor_y < MAP_H && map[neighbor_y][neighbor_x] == 0)
+			{
+				AStarNode *neighbor = &a_star_node_map[neighbor_y][neighbor_x];
+
+				if(!neighbor->visited)
+				{
+					int g = curr->g + 1;
+
+					if(!neighbor->search)
+					{
+						neighbor->x            = neighbor_x;
+						neighbor->y            = neighbor_y;
+						neighbor->g            = g;
+						neighbor->search       = true;
+						neighbor->next_in_path = curr;
+
+						int manhattan_x        = abs(target_x - neighbor_x);
+						int manhattan_y        = abs(target_y - neighbor_y);
+						int manhattan_distance = manhattan_x + manhattan_y;
+
+						neighbor->h = manhattan_distance;
+
+						to_search = a_star_node_insert(to_search, neighbor);
+					}else if(g < neighbor->g)
+					{
+						neighbor->g            = g;
+						neighbor->next_in_path = curr;
+					}
+				}
+			}
+		}
+	}
+	
+	return result;
+}
+
+AStarNode *a_star_path_find_old(int start_x, int start_y, int target_x, int target_y)
+{
+	AStarNode *result = NULL;
+
+	static AStarNode a_star_node_map[MAP_H][MAP_W];
+	memset(a_star_node_map, 0, MAP_W * MAP_H * sizeof(AStarNode));
+
+	a_star_node_map[start_y][start_x] = {start_x, start_y};
+
+	int        search_count             = 1;
+	AStarNode *to_search[MAP_W * MAP_H] = {&a_star_node_map[start_y][start_x]};
+
+	while(to_search)
+	{
+		int curr_idx = 0;
+		for(int to_search_idx = 0; to_search_idx < search_count; ++to_search_idx)
+		{
+			AStarNode *n    = to_search[to_search_idx];
+			AStarNode *curr = to_search[curr_idx];
+
+			int n_f    = n->g + n->h;
+			int curr_f = curr->g + curr->h;
+			if(n_f < curr_f || n_f == curr_f && n->h < curr->h)
+			{
+				curr_idx = to_search_idx;
+			}
+		}
+
+		AStarNode *curr = to_search[curr_idx];
+
+		if(curr->x == target_x && curr->y == target_y)
+		{
+			result = curr;
+			break;
+		}
+
+		curr->visited = true;
+		curr->search  = false;
+
+		to_search[curr_idx] = to_search[--search_count]; // Unordered removal
+
+		int neighbor_offsets_x[] = {1, 0, -1,  0};
+		int neighbor_offsets_y[] = {0, 1,  0, -1};
+
+		for(int neighbor_idx = 0; neighbor_idx < 4; ++neighbor_idx)
+		{
+			int neighbor_x = curr->x + neighbor_offsets_x[neighbor_idx];
+			int neighbor_y = curr->y + neighbor_offsets_y[neighbor_idx];
+
+			if(neighbor_x >= 0 && neighbor_x < MAP_W && neighbor_y >= 0 && neighbor_y < MAP_H && map[neighbor_y][neighbor_x] == 0)
+			{
+				AStarNode *neighbor = &a_star_node_map[neighbor_y][neighbor_x];
+
+				if(!neighbor->visited)
+				{
+					int g = curr->g + 1;
+
+					if(!neighbor->search)
+					{
+						neighbor->x            = neighbor_x;
+						neighbor->y            = neighbor_y;
+						neighbor->g            = g;
+						neighbor->search       = true;
+						neighbor->next_in_path = curr;
+
+						int manhattan_x        = abs(target_x - neighbor_x);
+						int manhattan_y        = abs(target_y - neighbor_y);
+						int manhattan_distance = manhattan_x + manhattan_y;
+
+						neighbor->h = manhattan_distance;
+
+						to_search[search_count++] = neighbor;
+					}else if(g < neighbor->g)
+					{
+						neighbor->g            = g;
+						neighbor->next_in_path = curr;
+					}
+				}
+			}
+		}
+	}
+	
+	return result;
+}
+
 struct StationType
 {
 	float r;
@@ -221,91 +467,6 @@ struct Station
 	int x;
 	int y;
 };
-
-struct Font
-{
-	float  baseline_advance;
-	GLuint texture;
-
-	stbtt_packedchar char_data['~' - ' '];
-};
-
-const int BITMAP_W = 512;
-const int BITMAP_H = 512;
-
-unsigned char tmp_bitmap[BITMAP_W * BITMAP_H];
-unsigned char ttf_buf[1 << 25];
-
-Font load_font(const char *filename)
-{
-	Font result = {};
-
-	FILE *file = fopen(filename, "rb");
-	if(file)
-	{
-		fread(ttf_buf, sizeof(char), array_count(ttf_buf), file);
-
-		stbtt_fontinfo font_info;
-		if(stbtt_InitFont(&font_info, ttf_buf, stbtt_GetFontOffsetForIndex(ttf_buf, 0)))
-		{
-			int   font_pixel_size = 16;
-			float font_scale      = stbtt_ScaleForPixelHeight(&font_info, font_pixel_size);
-
-			int ascent, descent, line_gap;
-			stbtt_GetFontVMetrics(&font_info, &ascent, &descent, &line_gap);
-
-			result.baseline_advance = font_scale * (ascent - descent + line_gap);
-
-			stbtt_pack_context pack_ctx;
-			stbtt_PackBegin(&pack_ctx, tmp_bitmap, BITMAP_W, BITMAP_H, 0, 1, NULL);
-			stbtt_PackSetOversampling(&pack_ctx, 2, 2);
-			stbtt_PackFontRange(&pack_ctx, ttf_buf, 0, font_pixel_size, ' ', array_count(result.char_data), result.char_data);
-			stbtt_PackEnd(&pack_ctx);
-
-			glGenTextures(1, &result.texture);
-			glBindTexture(GL_TEXTURE_2D, result.texture);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_W, BITMAP_H, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tmp_bitmap);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		}
-
-		fclose(file);
-	}
-
-	return result;
-}
-
-void draw_text(Font *font, float x, float y, float r, float g, float b, char *text)
-{
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, font->texture);
-
-	glBegin(GL_QUADS);
-
-	glColor3f(r, g, b);
-
-	while(*text)
-	{
-		stbtt_aligned_quad quad;
-		stbtt_GetPackedQuad(font->char_data, BITMAP_W, BITMAP_H, *text++ - ' ', &x, &y, &quad, false);
-
-		glTexCoord2f(quad.s0, quad.t0); glVertex2f(quad.x0, quad.y0);
-		glTexCoord2f(quad.s1, quad.t0); glVertex2f(quad.x1, quad.y0);
-		glTexCoord2f(quad.s1, quad.t1); glVertex2f(quad.x1, quad.y1);
-		glTexCoord2f(quad.s0, quad.t1); glVertex2f(quad.x0, quad.y1);
-	}
-	glEnd();
-}
-
-void draw_rect(float x, float y, float w, float h, float r, float g, float b)
-{
-	glColor3f(r, g, b);
-
-	glVertex2f(x,     y    );
-	glVertex2f(x + w, y    );
-	glVertex2f(x + w, y + h);
-	glVertex2f(x,     y + h);
-}
 
 void win_get_client_dim(HWND window, int *w, int *h)
 {
@@ -416,10 +577,6 @@ int WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int sho
 	QueryPerformanceCounter(&large_rng_seed);
 	unsigned int rng_seed = 69;//large_rng_seed.LowPart;
 
-	const int map_w = 128;
-	const int map_h = 128;
-	int       map[map_h][map_w] = {};
-
 	const int desired_station_count = 24;
 	Station stations[desired_station_count] = {};
 
@@ -440,8 +597,8 @@ int WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int sho
 		int w = type.w;
 		int h = type.h;
 
-		int bound_x = map_w - w - 2;
-		int bound_y = map_h - h - 2;
+		int bound_x = MAP_W - w - 2;
+		int bound_y = MAP_H - h - 2;
 
 		if(bound_x > 0 && bound_y > 0)
 		{
@@ -554,7 +711,7 @@ regenerate_facility:
 
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(0, map_w, map_h, 0, -1, 1);
+		glOrtho(0, MAP_W, MAP_H, 0, -1, 1);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 
@@ -580,74 +737,28 @@ regenerate_facility:
 					int target_x = station_b->x + type_b.door_offset_x;
 					int target_y = station_b->y + type_b.door_offset_y;
 
-					static AStarNode a_star_node_map[map_h][map_w];
-					memset(a_star_node_map, 0, map_w * map_h * sizeof(AStarNode));
+					AStarNode *old_path = a_star_path_find_old(start_x, start_y, target_x, target_y);
+					AStarNode *new_path = a_star_path_find_new(start_x, start_y, target_x, target_y);
 
-					a_star_node_map[start_y][start_x] = {start_x, start_y};
-
-					AStarBinaryHeapNode *to_search = (AStarBinaryHeapNode *)calloc(1, sizeof(AStarBinaryHeapNode));
-					to_search->n = &a_star_node_map[start_y][start_x];
-
-					while(to_search)
+					int tile_count = 0;
+					while(old_path && new_path)
 					{
-						a_star_iter_count++;
-
-						AStarNode *curr = to_search->n;
-
-						if(curr->x == target_x && curr->y == target_y)
+						if(old_path->x != new_path->x || old_path->y != new_path->y)
 						{
-							for(; curr; curr = curr->next_in_path)
-							{
-								draw_rect(curr->x, curr->y, 1, 1, 1, 1, 0);
-							}
-							break;
+							int breakpoint = 0;
 						}
 
-						curr->visited = true;
-						curr->search  = false;
+						old_path = old_path->next_in_path;
+						new_path = new_path->next_in_path;
 
-						to_search = a_star_node_remove(to_search);
-
-						int neighbor_offsets_x[] = {1, 0, -1,  0};
-						int neighbor_offsets_y[] = {0, 1,  0, -1};
-
-						for(int neighbor_idx = 0; neighbor_idx < 4; ++neighbor_idx)
-						{
-							int neighbor_x = curr->x + neighbor_offsets_x[neighbor_idx];
-							int neighbor_y = curr->y + neighbor_offsets_y[neighbor_idx];
-
-							if(neighbor_x >= 0 && neighbor_x < map_w && neighbor_y >= 0 && neighbor_y < map_h && map[neighbor_y][neighbor_x] == 0)
-							{
-								AStarNode *neighbor = &a_star_node_map[neighbor_y][neighbor_x];
-
-								if(!neighbor->visited)
-								{
-									int g = curr->g + 1;
-
-									if(!neighbor->search)
-									{
-										neighbor->x            = neighbor_x;
-										neighbor->y            = neighbor_y;
-										neighbor->g            = g;
-										neighbor->search       = true;
-										neighbor->next_in_path = curr;
-
-										int manhattan_x        = abs(target_x - neighbor_x);
-										int manhattan_y        = abs(target_y - neighbor_y);
-										int manhattan_distance = manhattan_x + manhattan_y;
-
-										neighbor->h = manhattan_distance;
-
-										to_search = a_star_node_insert(to_search, neighbor);
-									}else if(g < neighbor->g)
-									{
-										neighbor->g            = g;
-										neighbor->next_in_path = curr;
-									}
-								}
-							}
-						}
+						tile_count++;
 					}
+#if 0
+					for(; n; n = n->next_in_path)
+					{
+						draw_rect(n->x, n->y, 1, 1, 1, 1, 0);
+					}
+#endif
 				}
 			}
 		}
